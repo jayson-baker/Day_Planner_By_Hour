a170ac32a950cedae30c946f8245ba8933f2779e
Jayson Baker <bakej07@gmail.com>
2023-04-04 19:58:34 -0500

    Fixed the save button, now stores correct info

--- Develop/script.js
+++ Develop/script.js
@@ -1,35 +1,23 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 $(function () {
   let currentDate = new Date();
-
-  // TODO: Add a listener for click events on the save button. This code should
+  // A listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // local storage.
+  //
   let saveButton = $(".saveBtn");
-  saveButton.click(function (event) {
+  saveButton.on("click", function (event) {
     event.preventDefault();
-    let userInput = $("textarea").val();
+    let userInput = $(event.target).parent().children("textarea").val();
     console.log(userInput);
     let clickId = $(this).parent().attr("id");
     localStorage.setItem(clickId, `${userInput}`);
   });
 
+  // Code to apply the past, present, or future class to each time
+  // block by comparing the id to the current hour.
   //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-
   for (let i = 8; i <= 18; i++) {
-    let currentHour = currentDate.getHours(); // Returns a number between 0 and 23
+    let currentHour = currentDate.getHours();
     let idFinder = $(`[id='hour-${i}']`);
     console.log(idFinder);
     console.log(i);
@@ -49,8 +37,8 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
 
-  // TODO: Add code to display the current date in the header of the page.
-  // Display Date and Time to page
+  // Code to display the current date in the header of the page.
+  //
   setInterval(function () {
     let displayDate = $("#currentDay");
     displayDate.text(currentDate);
