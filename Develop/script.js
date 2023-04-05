8b457ec0ce1298edc285ec3caaf6351ac6337f2e
Jayson Baker <bakej07@gmail.com>
2023-04-04 20:22:08 -0500

    Added the getLocal storage functionality

--- Develop/script.js
+++ Develop/script.js
@@ -8,7 +8,6 @@ $(function () {
   saveButton.on("click", function (event) {
     event.preventDefault();
     let userInput = $(event.target).parent().children("textarea").val();
-    console.log(userInput);
     let clickId = $(this).parent().attr("id");
     localStorage.setItem(clickId, `${userInput}`);
   });
@@ -19,8 +18,6 @@ $(function () {
   for (let i = 8; i <= 18; i++) {
     let currentHour = currentDate.getHours();
     let idFinder = $(`[id='hour-${i}']`);
-    console.log(idFinder);
-    console.log(i);
     if (i < currentHour) {
       idFinder.removeClass("present future");
       idFinder.addClass("past");
@@ -32,10 +29,18 @@ $(function () {
       idFinder.addClass("present");
     }
   }
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+  // Code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements.
   //
+  for (let i = 8; i <= 18; i++) {
+    let textField = $(`[id='hour-${i}']`).children("textarea");
+    console.log(textField);
+    let savedText = localStorage.getItem(`hour-${i}`);
+    console.log(savedText);
+    if (savedText !== null) {
+      textField.text(savedText);
+    }
+  }
 
   // Code to display the current date in the header of the page.
   //
